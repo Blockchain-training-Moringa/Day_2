976d0679f96b2597d1d4ab487d77bddd8f284881
DevLaukey <devlaukeymwaura@gmail.com>
2022-09-02 17:00:41 +0300

    Added abi and register scripts

---
+++ .history/scripts/bankApp_20220902165458.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+
+async function main() {
+  const signers = await hre.ethers.getSigners();
+  const account0 = signers[0].address;
+  const BankApp = await hre.ethers.getContractFactory("BankApp");
+  const bankApp = await BankApp.deploy("Lois Lane");
+  await bankApp.deployed();
+
+  await bankApp.register(2331, account0, "Lois Lane", "A233casd", 0);
+
+  await bankApp.login();
+  await bankApp.deposit(20);
+  await bankApp.checkBalance(account0);
+  console.log(await bankApp.checkBalance(account0));
+}
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ abi/BankAppAbi.js
@@ -0,0 +1,8 @@
+module.exports[
+    "function deposit() public payable",
+"function checkBalance(address) public view returns (uint256)",
+"function login() public",
+"function register(uint256, address, string memory, string memory, uint256) public",
+"function withdraw(uint256) public",
+    
+]
